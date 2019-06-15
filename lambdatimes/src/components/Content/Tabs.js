9786a6d8bb3144a15f7edd3a0da5751f958b0c3a
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-06-15 13:10:59 -0500

    added index as key, will change later

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -5,16 +5,15 @@ import PropTypes from 'prop-types';
 
 
 const Tabs = props => {
-  console.log(props);
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/
-            props.tabs.map(tab => {
+            props.tabs.map((tab, index ) => {
               return (
-                <Tab tab={tab} selectTabHandler={props.selectTabHandler} selectedTab={props.selectedTab} />
+                <Tab tab={tab} selectTabHandler={props.selectTabHandler} selectedTab={props.selectedTab} key={index}/>
               )
             })
             }
