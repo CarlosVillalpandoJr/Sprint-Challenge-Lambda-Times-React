e226d85e9357d6224cf96266c87d441ee16247e2
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-06-17 21:09:16 -0500

    card filtering function working

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,16 +1,23 @@
 import React from 'react';
 
 const Tab = props => {
-  console.log(props.tab)
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+      let classChange; 
+      if(props.tab === props.selectedTab) {
+        classChange='tab active-tab';
+      }
+      else {
+        classChange='tab'
+      }
   return (
     <div
-      className={''}
-      onClick={() => {
+      className={'classChange'}
+      onClick={(event) => { props.selectTabHandler(props.tab);
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
+         console.log('im click why not work')
       }}
     >
       {props.tab.toUpperCase()}
